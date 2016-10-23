9c8ba6431b7ce31266a2c3716a1d6b41eb24ca3e
aasshhiisshh <ashpatelee@gmail.com>
2016-10-22 22:05:11 -0700

    Updated

---
+++ Speech_ recognition_done/Speech_recognition.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+'''
+I will update speech recognition with security lock.
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+'''
